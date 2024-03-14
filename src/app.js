3d6a791a06cd2d8d12170320d86f334d8e1c4383
Felipe Espana <faespana@espol.edu.ec>
2024-03-14 12:12:42 +0800

    second commit metodo get & status

--- src/app.js
+++ src/app.js
@@ -3,8 +3,23 @@ const express = require("express");
 const app = express();
 
 app.get("/api/v1/genetic-diseases", (req, res) => {
-  res.send("Hola mundo!");
+  return res.status(200).json({
+    message: "method get findAll",
+  });
+
+  // res.status(200).json([
+  //   {
+  //     enfermedad: "enfermedad1",
+  //   },
+  //   {
+  //     enfermedad: "enfermedad2",
+  //   },
+  // ]);
+
+  //res.send("Hola mundo!");
 });
+
+app.post("/api/v1/genetic-diseases", (req, res) => {});
 
 app.listen(3000, () => {
   console.log("El servidor esta escuchando en el puerto 4000");

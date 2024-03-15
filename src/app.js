0017f7d55a7193ae272f520bcaa97bd5a8c789a9
Felipe Espana <faespana@espol.edu.ec>
2024-03-15 23:31:42 +0800

    third commit metodo post

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
 const app = express();
+//Permite obtener el body en formato json
+app.use(express.json());
+//Permite obtener el body en formato url encoded
+app.use(express.urlencoded({ extended: true }));
 
+//Endpoint para buscar todas las enfermedades geneticas
 app.get("/api/v1/genetic-diseases", (req, res) => {
   return res.status(200).json({
     message: "method get findAll",
@@ -19,7 +24,34 @@ app.get("/api/v1/genetic-diseases", (req, res) => {
   //res.send("Hola mundo!");
 });
 
-app.post("/api/v1/genetic-diseases", (req, res) => {});
+//Endpoint para crear una enfermedad genetica
+app.post("/api/v1/genetic-diseases", (req, res) => {
+  //El postman simula lo que se hace con axios en el frontend;
+  console.log(req.body);
+  //Se puede tambien hacer de la siguiente forma;
+  const disease = req.body;
+
+  //Desestructurando
+  const { enfermedad, especie, raza } = req.body;
+
+  console.log(enfermedad);
+
+  return res.status(201).json({
+    message: "method post create",
+    data: req.body,
+    otherData: disease,
+  });
+});
+
+//Endpoint para buscar UNA enfermedad genetica
+app.get("/api/v1/genetic-diseases/:id", (req, res) => {
+  //Sino se llama el req.params no va a funcionar mi llamado por parametros
+  console.log(req.params);
+  return res.status(200).json({
+    message: "method get findOne",
+    id: req.params.id,
+  });
+});
 
 app.listen(3000, () => {
   console.log("El servidor esta escuchando en el puerto 4000");

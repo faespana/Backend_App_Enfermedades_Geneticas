118cb5a55edb65ef9b5129b7274291e4f16f4246
Felipe Espana <faespana@espol.edu.ec>
2024-03-18 01:04:22 +0800

    fifth commit crudfinished

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ app.use(express.json());
 //Permite obtener el body en formato url encoded
 app.use(express.urlencoded({ extended: true }));
 
-//Endpoint para buscar todas las enfermedades geneticas
-app.get("/api/v1/genetic-diseases", (req, res) => {
+//Mediante la funciones puedo pasarle por parametro como valor al metodo GET el callback "findAll"
+const findAll = (req, res) => {
   return res.status(200).json({
     message: "method get findAll",
   });
@@ -22,10 +22,9 @@ app.get("/api/v1/genetic-diseases", (req, res) => {
   // ]);
 
   //res.send("Hola mundo!");
-});
+};
 
-//Endpoint para crear una enfermedad genetica
-app.post("/api/v1/genetic-diseases", (req, res) => {
+const createOne = (req, res) => {
   //El postman simula lo que se hace con axios en el frontend;
   console.log(req.body);
   //Se puede tambien hacer de la siguiente forma;
@@ -41,36 +40,48 @@ app.post("/api/v1/genetic-diseases", (req, res) => {
     data: req.body,
     otherData: disease,
   });
-});
+};
 
-//Endpoint para buscar UNA enfermedad genetica
-app.get("/api/v1/genetic-diseases/:id", (req, res) => {
+const findOne = (req, res) => {
   //Sino se llama el req.params no va a funcionar mi llamado por parametros
   console.log(req.params);
   return res.status(200).json({
     message: "method get findOne",
     id: req.params.id,
   });
-});
+};
 
-//Endpoint para actualizar una enfermedad
-app.patch("/api/v1/genetic-diseases/:id", (req, res) => {
+const updateOne = (req, res) => {
   const { id } = req.params;
 
   return res.status(200).json({
     message: "method patch updateOne",
     id, //Puede ir solo id sin el : ya que solo tiene una cosa que desestructurar
   });
-});
+};
 
-app.delete("/api/v1/genetic-diseases/:id", (req, res) => {
+const eliminateOne = (req, res) => {
   const { id } = req.params;
 
   return res.status(200).json({
     message: "method delete eliminateOne",
     id,
   });
-});
+};
+
+//Endpoint para buscar todas las enfermedades geneticas
+app.get("/api/v1/genetic-diseases", findAll);
+
+//Endpoint para crear una enfermedad genetica
+app.post("/api/v1/genetic-diseases", createOne);
+
+//Endpoint para buscar UNA enfermedad genetica
+app.get("/api/v1/genetic-diseases/:id", findOne);
+
+//Endpoint para actualizar una enfermedad
+app.patch("/api/v1/genetic-diseases/:id", updateOne);
+
+app.delete("/api/v1/genetic-diseases/:id", eliminateOne);
 
 app.listen(3000, () => {
   console.log("El servidor esta escuchando en el puerto 4000");

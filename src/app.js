140b9477c2902a8bba4202c41cf9a1718e44777b
Felipe Espana <faespana@espol.edu.ec>
2024-03-25 16:05:47 +0800

    Creacion de Middlewares

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
+//IMPORTANDO EXPRESS
 const express = require("express");
 
+//CREANDO UNA CONSTANTE APP QUE TENDRA TODAS LAS FUNCIONALIDADES DE EXPRESS
 const app = express();
+
+//Creando un Middleware para evitar el DRY: Dont Repeat Yourself"
+const calculateRequestTime = (req, res, next) => {
+  const requestTime = new Date().toISOString();
+
+  req.requestTime = requestTime;
+  next();
+};
+
+//MIDDLEWARES 👇🏽
 //Permite obtener el body en formato json
 app.use(express.json());
 //Permite obtener el body en formato url encoded
 app.use(express.urlencoded({ extended: true }));
+//Permite enviar el tiempo de la solicitud (notese que no se debe ejecutar)
+app.use(calculateRequestTime);
 
+//DEFINICION DE FUNCIONES
 //Mediante la funciones puedo pasarle por parametro como valor al metodo GET el callback "findAll"
 const findAll = (req, res) => {
+  const requestTime = req.requestTime;
+  //Desestructurando, otra forma de llamar al requestTime
+  //const { requestTime } = req
   return res.status(200).json({
     message: "method get findAll",
+    requestTime,
   });
 
   // res.status(200).json([
@@ -30,6 +49,8 @@ const createOne = (req, res) => {
   //Se puede tambien hacer de la siguiente forma;
   const disease = req.body;
 
+  const { requestTime } = req;
+
   //Desestructurando
   const { enfermedad, especie, raza } = req.body;
 
@@ -39,36 +60,44 @@ const createOne = (req, res) => {
     message: "method post create",
     data: req.body,
     otherData: disease,
+    requestTime,
   });
 };
 
 const findOne = (req, res) => {
+  const { requestTime } = req;
   //Sino se llama el req.params no va a funcionar mi llamado por parametros
   console.log(req.params);
   return res.status(200).json({
     message: "method get findOne",
     id: req.params.id,
+    requestTime,
   });
 };
 
 const updateOne = (req, res) => {
   const { id } = req.params;
+  const { requestTime } = req;
 
   return res.status(200).json({
     message: "method patch updateOne",
     id, //Puede ir solo id sin el : ya que solo tiene una cosa que desestructurar
+    requestTime,
   });
 };
 
 const eliminateOne = (req, res) => {
   const { id } = req.params;
+  const { requestTime } = req;
 
   return res.status(200).json({
     message: "method delete eliminateOne",
     id,
+    requestTime,
   });
 };
 
+//DEFINIR LOS ENDPOINTS
 //Endpoint para buscar todas las enfermedades geneticas
 app.get("/api/v1/genetic-diseases", findAll);
 
@@ -81,8 +110,9 @@ app.get("/api/v1/genetic-diseases/:id", findOne);
 //Endpoint para actualizar una enfermedad
 app.patch("/api/v1/genetic-diseases/:id", updateOne);
 
+//Endpoint para eliminar una enfermedad
 app.delete("/api/v1/genetic-diseases/:id", eliminateOne);
 
 app.listen(3000, () => {
-  console.log("El servidor esta escuchando en el puerto 4000");
+  console.log("El servidor esta escuchando en el puerto 3000");
 });
